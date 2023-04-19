d964bf6e1ddcc4fcc5c3e9617ca2619ff4818534
Ravele Silva <ravele.cs@gmail.com>
2023-04-19 18:00:53 -0300

    Efeito de sombreamento

--- JavaScript/menu-mobile.js
+++ JavaScript/menu-mobile.js
@@ -1,9 +1,11 @@
 const menuHamburger = document.querySelector('.menu-hamburger')
 const headerNav = document.querySelector('.header-nav')
+const backShadow = document.querySelector('.backshadow')
 
 function activateMenu(){
     headerNav.classList.toggle('active')
     menuHamburger.classList.toggle('active')
+    backShadow.classList.toggle('active')
 }
 
 menuHamburger.addEventListener('click', activateMenu)
